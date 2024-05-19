fc1a4e2f4d3b27bf44cd2e225a03f0b67d965936
Iridias <iridias@tiphares.de>
2024-05-19 12:26:54 +0200

    updated install-script to use kpackagetool6 instead of old plasmapkg2

--- install.sh
+++ install.sh
@@ -2,13 +2,19 @@
 
 cd "${0%/*}"
 
-# Remove the Simple System Monitor plasmoid.
-plasmapkg2 -r ./plasmoid > /dev/null
 
-# Install the Simple System Monitor plasmoid.
-plasmapkg2 -t plasmoid -i ./plasmoid
+ACTION="-i"
+
+if [[ $(kpackagetool6 -l -t "Plasma/Applet" | grep "org.kde.simpleMonitor" | wc -l) -gt 0 ]]; then
+	ACTION="-u"
+fi
+
+
+# Install or upgrade the Simple System Monitor plasmoid.
+kpackagetool6 -t "Plasma/Applet" ${ACTION} ./plasmoid
 
 # Install the Simple System Monitor icon.
 ICON_PATH=${HOME}/.local/share/icons/hicolor/scalable/apps/
 mkdir -p ${ICON_PATH}
 cp plasmoid/contents/images/simpleMonitor_icon.svg ${ICON_PATH}
+
